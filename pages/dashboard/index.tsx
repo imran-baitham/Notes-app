3aa4d7531b94013a19664da67e35bbeaf6da0fd7
imran-baitham <imranbaitham001@gmail.com>
2023-02-08 14:46:06 +0500

    new setup code

---
+++ pages/dashboard/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function index() {
+  return (
+    <div className="">
+      <div className="">
+        <h1>DashBoard</h1>
+      </div>
+    </div>
+  )
+}
+
+export default index
